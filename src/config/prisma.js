e7b84455a60c5eb61f7074f718cd058bd7d5166f
shushant <shushantkumar0625@gmail.com>
2026-08-27 00:20:14 +0530

    change prisma log

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -2,9 +2,14 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
+import pg from "pg";
+
+const { Pool } = pg;
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
